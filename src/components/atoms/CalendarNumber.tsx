1bbcbd827ec67adaa0b0e9faaec65d986953835a
Ruben <donowenrt@gmail.com>
2022-05-28 00:35:25 +0200

    created calendarNumber

---
+++ src/components/atoms/CalendarNumber.tsx
@@ -0,0 +1,43 @@
+import { Flex, Box } from "@chakra-ui/react";
+
+interface CalendarNumberProps {
+  numberToDisplay: string | number;
+}
+
+const CalendarNumber = ({ numberToDisplay }: CalendarNumberProps) => {
+  return (
+    <Box position="relative">
+      <Flex
+        w={20}
+        h={28}
+        bg="linear-gradient(0deg, #383838 49%, white 49%, white 51%, #383838 51%)"
+        justify="center"
+        align="center"
+        fontSize="7xl"
+        fontWeight="extrabold"
+        color="yellow.100"
+        borderRadius="2px"
+        _after={{
+          content: "''",
+          height: 6,
+          width: 1,
+          background: "gray.500",
+          position: "absolute",
+          right: "100%",
+        }}
+        _before={{
+          content: "''",
+          height: 6,
+          width: 1,
+          background: "gray.500",
+          position: "absolute",
+          left: "100%",
+        }}
+      >
+        {numberToDisplay}
+      </Flex>
+    </Box>
+  );
+};
+
+export default CalendarNumber;
