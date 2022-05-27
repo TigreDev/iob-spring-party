bbf5537c5847f36618d96483c111d55014a32718
Ruben <donowenrt@gmail.com>
2022-05-27 13:01:09 +0200

    changed image to svg

--- src/components/atoms/iobLogo.tsx
+++ src/components/atoms/iobLogo.tsx
@@ -4,12 +4,7 @@ import type { BoxProps } from "@chakra-ui/react";
 const IobLogo = (props: BoxProps) => {
   return (
     <Box {...props}>
-      <Img
-        alt="iobLogo"
-        src={"/image/ioBuilders_original.png"}
-        w="full"
-        h="full"
-      />
+      <Img alt="iobLogo" src={"/image/ioBuilders_dark.svg"} w="full" h="full" />
     </Box>
   );
 };
