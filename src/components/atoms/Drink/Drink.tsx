49f2a4c0c8f1a925e366505fd8c5c2735899f442
Ruben <donowenrt@gmail.com>
2022-05-29 01:43:49 +0200

    all done

--- src/components/atoms/Drink/Drink.tsx
+++ src/components/atoms/Drink/Drink.tsx
@@ -1,22 +1,65 @@
+import { useState } from "react";
 import { Box, BoxProps } from "@chakra-ui/react";
 import Lottie from "lottie-react";
 import lemonade from "./drink.json";
 import orange from "./orangeDrink.json";
+import champagne from "./champagne.json";
+import pomerade from "./pomerade.json";
+import coke from "./coke.json";
+
+type breverageType = "lemonade" | "orange" | "champagne" | "pomerade" | "coke";
 
 interface DrinkProps {
   containerStyle: BoxProps;
-  breverage: "lemonade" | "orange";
+  breverage: breverageType;
 }
 
 const Drink = ({ containerStyle, breverage }: DrinkProps) => {
   const drinks = {
     lemonade,
     orange,
+    champagne,
+    pomerade,
+    coke,
+  };
+  const drinkArray = Object.keys(drinks);
+
+  const [currentDrink, setDrink] = useState<breverageType>(breverage);
+  const [drinksLeft, setDrinksLeft] = useState(
+    Object.keys(drinks).filter((drink) => drink !== breverage)
+  );
+
+  const getRandomDrink = (drinksLeft: string[]) => {
+    const lengthOfDrinks = drinksLeft.length;
+    let randomDrink;
+    let nextDrink: string;
+    let newLeftDrinks;
+
+    if (lengthOfDrinks === 1) {
+      randomDrink = Math.floor(Math.random() * 5);
+      nextDrink = drinkArray[randomDrink];
+      newLeftDrinks = drinkArray.filter((drink) => drink !== nextDrink);
+    } else {
+      randomDrink = Math.floor(Math.random() * lengthOfDrinks);
+      nextDrink = drinksLeft[randomDrink];
+      newLeftDrinks = drinksLeft.filter((drink) => drink !== nextDrink);
+    }
+    return { nextDrink, newLeftDrinks };
+  };
+
+  const nextDrink = () => {
+    const { nextDrink, newLeftDrinks } = getRandomDrink(drinksLeft);
+    setDrink(nextDrink as breverageType);
+    setDrinksLeft(newLeftDrinks);
   };
 
   return (
     <Box {...containerStyle}>
-      <Lottie animationData={drinks[breverage]} loop={false} />
+      <Lottie
+        animationData={drinks[currentDrink]}
+        loop={false}
+        onClick={() => nextDrink()}
+      />
     </Box>
   );
 };
--- src/pages/landing.tsx
+++ src/pages/landing.tsx
@@ -1,26 +1,39 @@
-import { Box, Center, Flex, Heading, Text } from "@chakra-ui/react";
+import { Box, Flex, Heading, Text, useBreakpointValue } from "@chakra-ui/react";
 import Drink from "../components/atoms/Drink/Drink";
 import IobLogo from "../components/atoms/iobLogo";
 import Timer from "../components/molecule/Timer";
 
 function Landing() {
   return (
-    <Center minH="100vh" bgColor="#e1e9ec" w="full">
-      <Flex align="center" direction="column" alignSelf="start" zIndex="10">
+    <Flex minH="100vh" bgColor="#e1e9ec" w="full" direction="column">
+      <Flex align="center" direction="column" zIndex="10">
         <IobLogo w={{ md: "25em" }} h="auto" mt={{ base: 10, md: 20 }} />
         <Heading
           color="#2c3e50"
           fontSize={{ base: "5rem", md: "10rem" }}
           fontFamily="handWritted"
           fontWeight="initial"
+          lineHeight="100%"
         >
           SPRING PARTY
         </Heading>
         <Box>
           <Timer />
         </Box>
+        <Text
+          color="#2c3e50"
+          fontSize="xl"
+          fontWeight="bold"
+          textAlign="center"
+          mt={{ base: 4, md: 8 }}
+        >
+          {useBreakpointValue({ base: "👇🏻", md: "👈🏻" })} While waiting you can
+          serve you another drink!
+        </Text>
+      </Flex>
+      <Flex justify="center">
         <Drink
-          breverage="orange"
+          breverage="pomerade"
           containerStyle={{
             w: { base: 80, md: "30rem" },
             position: { base: "relative", md: "absolute" },
@@ -29,19 +42,18 @@ function Landing() {
             zIndex: "1",
           }}
         />
-        <Text
-          fontWeight="thin"
-          color="black"
-          fontSize="xs"
-          position={{ base: "relative", md: "absolute" }}
-          bottom={{ md: 0 }}
-          mt={{ base: 8, md: 0 }}
-        >
-          © 2022. All Rights reserved. ioB Party Timer made with 💖 by Ruben
-          Tigre
-        </Text>
       </Flex>
-    </Center>
+      <Text
+        fontWeight="thin"
+        color="black"
+        fontSize="xs"
+        position={{ base: "relative", md: "absolute" }}
+        bottom={{ md: 0 }}
+        mt={{ base: 8, md: 0 }}
+      >
+        © 2022. All Rights reserved. ioB Party Timer made with 💖 by Ruben Tigre
+      </Text>
+    </Flex>
   );
 }
 
