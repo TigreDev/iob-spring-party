c0f6c5f23ba360c1f5d0152f54bbb36904a1f457
Ruben <donowenrt@gmail.com>
2022-05-30 09:23:56 +0200

    changed padding and footer

--- src/pages/landing.tsx
+++ src/pages/landing.tsx
@@ -8,12 +8,13 @@ import Footer from "../components/atoms/Footer";
 function Landing() {
   return (
     <Flex minH="100vh" bgColor="#e1e9ec" w="full" direction="column">
-      <Flex align="center" direction="column">
+      <Flex align="center" direction="column" h="full">
         <IobLogo w={{ md: "25em" }} h="auto" mt={{ base: 10, md: 20 }} />
         <Heading
           fontSize={{ base: "5rem", md: "10rem" }}
           fontFamily="handWritted"
           textTransform="uppercase"
+          mt={{base: 2, md: 7}}
         >
           {partyConfig.partyName}
         </Heading>
@@ -27,8 +28,8 @@ function Landing() {
             alignSelf: {md: "flex-start"}
           }}
         />
+        <Footer />
       </Flex>
-      <Footer />
     </Flex>
   );
 }
